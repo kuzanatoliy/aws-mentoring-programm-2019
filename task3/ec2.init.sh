9c84fd53a178b7e4e290fb86a114eb6062e18f7f
kuzanatoliy <njkznu@gmail.com>
2019-10-05 23:20:24 +0300

    Task3: Add script for EC2 instance

---
+++ task3/ec2.init.sh
@@ -0,0 +1,5 @@
+sudo yum install java-1.8.0-openjdk -y
+sudo yum install httpd -y
+sudo systemctl enable httpd
+sudo systemctl start httpd
+sudo aws s3 cp s3://ak.staticwebsitetask2 /var/www/html --recursive
