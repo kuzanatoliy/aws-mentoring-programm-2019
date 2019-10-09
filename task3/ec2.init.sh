71ea8dd485cf6ef77b96af3372491401a7911e79
Anatoli Kuzmiankou <Anatoli_Kuzmiankou@epam.com>
2019-10-09 11:45:19 +0300

    Task3: Fix header for ec2.init.sh

--- task3/ec2.init.sh
+++ task3/ec2.init.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-sudo yum install java-1.8.0-openjdk -y
-sudo yum install httpd -y
-sudo systemctl enable httpd
-sudo systemctl start httpd
-sudo aws s3 cp s3://ak.staticwebsitetask2 /var/www/html --recursive
+yum install java-1.8.0-openjdk -y
+yum install httpd -y
+systemctl enable httpd
+systemctl start httpd
+aws s3 cp s3://ak.staticwebsitetask2 /var/www/html --recursive
